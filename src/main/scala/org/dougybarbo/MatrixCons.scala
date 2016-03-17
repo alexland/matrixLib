ced4e13150ba901b3e45307011ad31de0568e540
alexland <doug.ybarbo@gmail.com>
2016-03-16 20:06:15 -0700

    added matrix constructor from random numbers

---
+++ src/main/scala/org/dougybarbo/MatrixCons.scala
@@ -0,0 +1,24 @@
+
+package org.dougybarbo.MatrixLib
+
+import java.util.concurrent.{
+  Callable,
+  Executors
+}
+
+import collection.mutable.ArrayBuffer
+
+import scala.util.{
+  Random ⇒ RND
+}
+
+object MatrixCons {
+
+  def rnd(m: Int, n: Int) = {
+    val row = (m: Int) ⇒ Seq.fill(m)(RND.nextDouble).toArray
+    val mx = (m: Int, n: Int, f: Int ⇒ Array[Double]) ⇒ Seq.fill(m)(f(n))
+      .toArray
+    new Matrix(mx(m, n, row))
+  }
+
+}
