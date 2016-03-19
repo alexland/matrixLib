305c6c0c771d818878d968d87a92dac5f0b36f5b
alexland <doug.ybarbo@gmail.com>
2016-03-19 11:07:10 -0700

    added additional test for random matrix constructor

--- src/test/scala/org/dougybarbo/MatrixSpec.scala
+++ src/test/scala/org/dougybarbo/MatrixSpec.scala
@@ -11,7 +11,7 @@ class MatrixSpec extends FlatSpec {
 
   implicit val ts = ThreadStrategy.SameThreadStrategy
 
-  "a Matrix" should "have a column rank and a row rank and a shape" in {
+  "a matrix" should "have a column rank and a row rank and a shape" in {
     val a = new Matrix(Array(Array(4, 3, 9), Array(5, 2, 7)))
     assert(a.colRank === 3)
     assert(a.rowRank === 2)
@@ -23,6 +23,15 @@ class MatrixSpec extends FlatSpec {
     val a2 = new Matrix(Array(Array(4), Array(7), Array(3)))
     val res = MatrixMul.mmul(a1, a2)
     assert(res.shape === (2, 1))
+  }
+
+  "a matrix" should "be created from a matrix constructor" in {
+    val a1 = MatrixCons.rnd(8, 5)
+    val a2 = MatrixCons.rnd(5, 2)
+    val res = MatrixMul.mmul(a1, a2)
+    assert(a1.shape === (8, 5))
+    assert(a2.shape === (5, 2))
+    assert(res.shape === (8, 2))
   }
 
 }
