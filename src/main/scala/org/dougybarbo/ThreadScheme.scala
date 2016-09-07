6ede9b17e940a77489df62ce692c4c5e74e6a1ed
alexland <doug.ybarbo@gmail.com>
2016-09-06 22:14:10 -0700

    minor revision to multithread obj

--- src/main/scala/org/dougybarbo/ThreadScheme.scala
+++ src/main/scala/org/dougybarbo/ThreadScheme.scala
@@ -1,9 +1,10 @@
 
+
 package org.dougybarbo.MatrixLib
 
 import java.util.concurrent.{
-  Callable,
-  Executors
+	Callable,
+	Executors
 }
 
 import collection.mutable.ArrayBuffer
@@ -11,46 +12,47 @@ import collection.mutable.ArrayBuffer
 /**
  * defines an interface for how to execute functions
  */
-trait ThreadStrategy {
-  /**
-   * 	pass in a fn that returns a value of type A
-   * 	returns a fn that returns a value of type A
-   * 	the actual function may execute on another thread
-   */
-  def execute[A](f: Function0[A]): Function0[A]
+trait ThreadScheme {
+	/**
+	 * 	pass in a fn that returns a value of type A
+	 * 	returns a fn that returns a value of type A
+	 * 	the actual function may execute on another thread
+	 */
+	def execute[A](func: Function0[A]): Function0[A]
 }
 
 /**
- * companion object to ThreadStrategy trait
+ * companion object to ThreadScheme trait
  */
-object ThreadStrategy {
-  /**
-   * will execute all functions on single thread (local thread)
-   */
-  object SameThreadStrategy extends ThreadStrategy {
-    def execute[A](func: Function0[A]) = func
-  }
+object ThreadScheme {
+	/**
+	 * will execute all functions on the local thread
+	 */
+	object LocalThread extends ThreadScheme {
+		def execute[A](func: Function0[A]) = func
+	}
 
-  /**
-   * will execute all functions within a thread pool
-   */
-  object ThreadPoolStrategy extends ThreadStrategy {
-    val pool = Executors.newFixedThreadPool(
-      java.lang.Runtime.getRuntime.availableProcessors
-    )
-    def execute[A](f: Function0[A]) = {
-      // submit a callable class to the thread pool
-      val future = pool.submit(new Callable[A] {
-        def call(): A = {
-          Console.println(
-            "executing function on thread: " + Thread.currentThread.getName
-          )
-          f()
-        }
-      })
-      // create a function that will block when called and
-      // wait for the defered thread to finish execution
-      () ⇒ future.get()
-    }
-  }
+	/**
+	 * concurrent/multi-threaded option
+	 * will execute all functions within a thread pool
+	 */
+	object MultiThread extends ThreadScheme {
+		val pool = Executors.newFixedThreadPool(
+			java.lang.Runtime.getRuntime.availableProcessors
+		)
+		def execute[A](func: Function0[A]) = {
+			// submit a callable class to the thread pool
+			val future = pool.submit(new Callable[A] {
+				def call():A = {
+					Console.println(
+						"executing function on thread: " + Thread.currentThread.getName
+					)
+					func()
+				}
+			})
+			// create a function that will block when called and
+			// wait for the defered thread to finish execution
+			() ⇒ future.get()
+		}
+	}
 }
